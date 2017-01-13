dc1ea0fb01f2d6c0cfb96c4626e98fb58d2867a2
Richard Strutt <richardastrutt@gmail.com>
2017-01-13 11:24:13 +0800

    v1.3 commit

--- src/app.js
+++ src/app.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react';
-//import firebase from 'firebase';
+import firebase from 'firebase';
 import { View } from 'react-native';
-import { Header } from './components/common';
+import { Button, Header, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
-  // componentWillMount() {
-  //   firebase.initializeApp({
-  //     apiKey: 'AIzaSyB-qVTVdrp9KtBVzvHQvsKz4wJXKmuMQy4',
-  //     authDomain: 'auth-tutorial-aabab.firebaseapp.com',
-  //     databaseURL: 'https://auth-tutorial-aabab.firebaseio.com',
-  //     storageBucket: 'auth-tutorial-aabab.appspot.com',
-  //     messagingSenderId: '710595420208'
-  //   });
-  // }
+  state = {
+    loggedIn: null
+  };
+
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB-qVTVdrp9KtBVzvHQvsKz4wJXKmuMQy4',
+      authDomain: 'auth-tutorial-aabab.firebaseapp.com',
+      databaseURL: 'https://auth-tutorial-aabab.firebaseio.com',
+      storageBucket: 'auth-tutorial-aabab.appspot.com',
+      messagingSenderId: '710595420208'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <View style={{ flexDirection: 'row' }}>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </View>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size='large' />;
+    }
+  }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,60 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import firebase from 'firebase';
-import { Button, Card, CardSection, Input } from './common';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 export default class LoginForm extends Component {
   state = {
     email: '',
+    error: '',
+    loading: false,
     password: ''
   };
 
   onButtonPress() {
     const { email, password } = this.state;
 
+    this.setState({ error: '', loading: true });
+
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
       .catch(() => {
-        firebase.auth().createUserWithEmailAndPassword(email, password);
+        firebase
+          .auth()
+          .createUserWithEmailAndPassword(email, password)
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
       });
+  }
+
+  onLoginFail() {
+    this.setState({
+      loading: false,
+      error: 'Authentication Failed'
+    });
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size='small' />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log In
+      </Button>
+    );
   }
 
   render() {
@@ -41,12 +79,22 @@ export default class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button onPress={this.onButtonPress.bind(this)}>
-            Log In
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
